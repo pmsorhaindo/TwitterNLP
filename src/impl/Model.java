862b998ae8701c2404caf69902e40ec5793cb220
Mikey <pms10@uni.brighton.ac.uk>
2014-06-26 20:37:15 +0100

    crude Nth most method - semi tested

--- src/impl/Model.java
+++ src/impl/Model.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import util.BasicFileIO;
+import util.Util;
 import edu.berkeley.nlp.util.ArrayUtil;
 import edu.berkeley.nlp.util.Triple;
 import edu.stanford.nlp.math.ArrayMath;
@@ -141,6 +142,18 @@ public class Model {
 		
 	}
 	
+	public String priArr(int[] arr){
+		String x = "arr: ";
+		for (int i =0; i<arr.length; i++)
+		{
+			x+=arr[i]+", ";
+		}
+		
+		x += " len: "+arr.length;
+		return x;
+		
+	}
+	
 	// TODO make this nicer
 	public int nargmax(double[] vals, int n)
 	{
@@ -182,13 +195,21 @@ public class Model {
 		
 		//initialization
 		vit[0]=labelScores;
+		
 		for (int k=0; k < numLabels; k++){
+			//start marker for all labels 
 			bptr[0][k]=startMarker();
 		}
+		
+		System.out.println("Initial back pointer array: " + priArr(bptr[0]));
+		
 		for (int t=1; t < T; t++){
+			System.out.println(">>>>Token: "+t);
 			double[][] prevcurr = new double[numLabels][numLabels];
 			for (int s=0; s < numLabels; s++){
+				System.out.println("labelScores["+s+"]"+labelScores[s]);
 				computeVitLabelScores(t, s, sentence, prevcurr[s]);
+				System.out.println("prevcurr["+s+"] "+ priArr(prevcurr[s]));
 				ArrayUtil.logNormalize(prevcurr[s]);
 				prevcurr[s] = ArrayUtil.add(prevcurr[s], labelScores[s]);
 			}
@@ -232,6 +253,14 @@ public class Model {
 			}
 			System.out.println();
 		}
+		
+		Util u = new Util();
+		double[] d = {11.0,21.0,23.0,4.0};
+		System.out.println(priArr(d));
+		System.out.println("nth largest test::: "+u.nthLargest(1, d));
+		System.out.println("nth largest test::: "+u.nthLargest(2, d));
+		System.out.println("nth largest test::: "+u.nthLargest(3, d));	
+		System.out.println("nth largest test::: "+u.nthLargest(4, d));
 
 	}
 	
@@ -240,8 +269,56 @@ public class Model {
 	 * then on each of the runs that splits switching back to finding the maximum path.
 	 * Is this cool? 
 	 */
-	private void splitViterbiDecode() {
+	private void splitViterbiDecode (ModelSentence sentence) {
+		int T = sentence.T;
+		sentence.labels = new int[T];
+		int[][] bptr = new int[T][numLabels];
+		double[][] vit = new double[T][numLabels];
+		double[] labelScores = new double[numLabels];
 		
+		//System.out.println("start Marker = "+ startMarker());
+		computeVitLabelScores(0, startMarker(), sentence, labelScores);
+		//System.out.println("label score init: \n" + priArr(labelScores));
+		ArrayUtil.logNormalize(labelScores);
+		//System.out.println("label score init (log norm'd): \n" + priArr(labelScores));
+		
+		//initialization
+		vit[0]=labelScores;
+		
+		for (int k=0; k < numLabels; k++){
+			//start marker for all labels 
+			bptr[0][k]=startMarker();
+		}
+		
+		System.out.println("Initial back pointer array: " + priArr(bptr[0]));
+		
+		for (int t=1; t < T; t++){
+			
+			double[][] prevcurr = new double[numLabels][numLabels];
+			for (int s=0; s < numLabels; s++){
+				computeVitLabelScores(t, s, sentence, prevcurr[s]);
+				System.out.println("prevcurr["+s+"] "+ priArr(prevcurr[s]));
+				ArrayUtil.logNormalize(prevcurr[s]);
+				prevcurr[s] = ArrayUtil.add(prevcurr[s], labelScores[s]);
+			}
+			for (int s=0; s < numLabels; s++){
+				double[] sprobs = getColumn(prevcurr, s);
+				bptr[t][s] = ArrayUtil.argmax(sprobs);
+				vit[t][s] = sprobs[bptr[t][s]];	
+			}
+			labelScores=vit[t];
+		}
+		sentence.labels[T-1] = ArrayUtil.argmax(vit[T-1]);
+		//System.out.print(labelVocab.name(sentence.labels[T-1]));
+		//System.out.println(" with prob: "+Math.exp(vit[T-1][sentence.labels[T-1]]));
+		int backtrace = bptr[T-1][sentence.labels[T-1]];
+		for (int i=T-2; (i>=0)&&(backtrace != startMarker()); i--){ //termination
+			sentence.labels[i] = backtrace;
+			//System.err.println(labelVocab.name(backtrace)
+				//+" with prob: "+Math.exp(vit[i][backtrace]));
+			backtrace = bptr[i][backtrace];
+		}
+		assert (backtrace == startMarker());
 	}
 
 	private double[] getColumn(double[][] matrix, int col){
@@ -259,7 +336,7 @@ public class Model {
 	}
 
 	/** Computes unnormalized log-potentials.
-	 * CLOBBERS labelScores **/
+	 * CLOBBERS labelScores **/ // slang for overwrites
 	public void computeLabelScores(int t, ModelSentence sentence, double[] labelScores) {
 		Arrays.fill(labelScores, 0);
 		computeBiasScores(labelScores);
@@ -269,7 +346,9 @@ public class Model {
 	public void computeVitLabelScores(int t, int prior, ModelSentence sentence, double[] labelScores) {
 		Arrays.fill(labelScores, 0);
 		computeBiasScores(labelScores);
+		System.out.println("prior = " + prior);
 		viterbiEdgeScores(prior, sentence, labelScores);
+		System.out.println("t = " + t);
 		computeObservedFeatureScores(t, sentence, labelScores);
 	}
 
--- src/util/Util.java
+++ src/util/Util.java
@@ -28,4 +28,51 @@ public class Util {
 	}
 	//	public static void p(int[][] x) { p(Arrays.toString(x)); }
 	public static void p(String x) { System.out.println(x); }
+
+	public void squeeze(double val,int position,double[] squeezedArr)
+	{
+		boolean endReached = false;
+		double hold = 0;
+		double nextVal = val; 
+		if(position>squeezedArr.length) endReached = true;
+		
+		while(!endReached)
+		{
+			hold = squeezedArr[position];
+			squeezedArr[position] = nextVal;
+			nextVal = hold;
+			position ++;
+			if (position>=squeezedArr.length) endReached = true;
+		}
+	}
+	
+	public int nthLargest(int n, double[] inputArr){
+		
+		double[] record = new double[n];
+		Arrays.fill(record, Double.MIN_VALUE);
+		int hIndex = 0;
+		
+		for(int i = 0; i<inputArr.length; i++)
+		{
+			for (int j = 0; j<record.length; j++)
+			{
+				if(inputArr[i]>record[j])
+				{
+					System.out.println("Squeeze record");
+					squeeze(inputArr[i],j,record);
+					p(record);
+					break;
+				}
+			}
+		}
+		
+		for (int i =0;i<inputArr.length; i++)
+		{
+			if(record[n-1]==inputArr[i])
+			{
+				return i;
+			}
+		}
+		return 0;
+	}
 }
