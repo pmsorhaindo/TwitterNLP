0ae1089c35a81ac1ecb0dcf4ec9c1de3e5e655b9
Mikey <pmsorhaindo@gmail.com>
2014-07-25 15:26:31 +0100

    File Splitting functional
    Cross Validation progress

--- src/eval/cv/CrossValidator.java
+++ src/eval/cv/CrossValidator.java
@@ -1,6 +1,9 @@
 package eval.cv;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.Arrays;
 
 import util.FileSplitter;
 
@@ -8,13 +11,31 @@ public class CrossValidator {
 
 	public CrossValidator(){
 		FileSplitter splits = new FileSplitter();
-		try {
-			splits.split("daily547.conll", 4);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		splits.readInAllTweetsSplit("/Volumes/LocalDataHD/ps324/Documents/workspace/TwitterNLP/bin/data/daily547.conll", 10, 5);
+		File path = new File("/Volumes/LocalDataHD/ps324/Documents/workspace/TwitterNLP/bin/data/");
+		trainModels(path,"daily547_split_");
+		
 	}
 	
+	private void trainModels(File path,String fileName) {
+
+		
+		String[] directories = path.list(new FilenameFilter() {
+		  @Override
+		  public boolean accept(File current, String name) {
+		    return new File(current, name).isDirectory();
+		  }
+		});
+		System.out.println(Arrays.toString(directories));
+		
+		for (int i = 0; i<directories.length; i++)
+		{
+			File f = new File(path,directories[i]);
+			System.out.println("file "+i+": "+f.toString());
+		}
+		
+	}
+
 	public static void main(String[] args) throws IOException {
 		CrossValidator cv = new CrossValidator();
 	}
--- src/util/FileSplitter.java
+++ src/util/FileSplitter.java
@@ -10,72 +10,147 @@ import java.io.IOException;
 import java.io.LineNumberReader;
 import java.io.RandomAccessFile;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 import org.apache.commons.io.FileUtils;
 
 public class FileSplitter {
 
-	public FileSplitter(){
-		
-	}
-	
-		public static void split(String fileName,int numSplits) {
-			
-			LineNumberReader lnr;
-			int numOfLines = 0;
-			try {
-				lnr = new LineNumberReader(new FileReader(new File(fileName)));
+	public FileSplitter() {
 
-				lnr.skip(Long.MAX_VALUE);
-				System.out.println(lnr.getLineNumber());
-				numOfLines = lnr.getLineNumber();
-				// Finally, the LineNumberReader object should be closed to prevent resource leak
-			lnr.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			assert numOfLines > 0; //TODO make sure there are no empty files being split.
-			
-			int minLinesforSplit = numOfLines / numSplits;
-			
-			FileReader fr = null;
-			try {
-				fr = new FileReader("daily547.conll");
-			} catch (FileNotFoundException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			
-			int lineCounter = 0;
-			try (BufferedReader br = new BufferedReader(fr))
-			{
-				
-				String sCurrentLine;
-				int fileNum = 1;
-				while ((sCurrentLine = br.readLine()) != null) {
-					//
-					lineCounter ++;
-					String sFileName = "daily547_"+fileNum+".conll";
-					
-					FileUtils.write(new File(sFileName), sCurrentLine+"\n", Charset.defaultCharset(), true);
-					
-					System.out.println(sCurrentLine);
-					
-					if(lineCounter > minLinesforSplit && sCurrentLine.length()<1){
-						lineCounter = 0;
-						fileNum++;
-						System.out.println("split!");
-					}
-					
+	}
+
+	public void readInAllTweetsSplit(String fileName, int numSplits, int numReps) {
+
+		int lineCounter = 0;
+		int tweetNum = 0;
+		ArrayList<ArrayList<String>> tweets = new ArrayList<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+
+			String sCurrentLine;
+			ArrayList<String> tweet = new ArrayList<>();
+			while ((sCurrentLine = br.readLine()) != null) {
+
+				if (sCurrentLine.length() < 1) {
+					tweets.add(tweet);
+					tweetNum++;
+					tweet = new ArrayList<>();
 				}
-	 
-			} catch (IOException e) {
-				e.printStackTrace();
-			} 
-	 
+				tweet.add(sCurrentLine);
+
+			}
+			System.out.println("tweetNum: " + tweetNum);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-	
+
+		ArrayList<ArrayList<Integer>> indexes = new ArrayList<>();
+		ArrayList<Integer> firstIndexes = new ArrayList<>();
+
+		for (int i = 0; i < tweetNum; i++) {
+			firstIndexes.add(i);
+		}
+
+		for (int i = 0; i < numReps; i++) {
+			ArrayList<Integer> nextIndexes = new ArrayList<>();
+			nextIndexes.addAll(firstIndexes);
+			Collections.shuffle(nextIndexes, new Random(i));
+			indexes.add(nextIndexes);
+		}
+		
+		// write model files
+		// for each randomization list
+		for (int i = 0; i < indexes.size(); i++) {
+			//System.out.println(indexes.get(i).toString());
+			int splitSize = tweetNum/numSplits;
+			int tweetCount = 0;
+			int splitCount =0;
+			for (int j= 0; j<indexes.get(i).size(); j++)
+			{
+				if(tweetCount>splitSize && splitCount+1 < numSplits)
+				{
+					splitCount ++;
+					tweetCount = 0;
+				}
+				String sFileName = "/Volumes/LocalDataHD/ps324/Documents/workspace/TwitterNLP/bin/data/Run_"+(i+1)+"/daily547_split_"
+						+ (splitCount+1) + ".conll";
+				//System.out.println(indexes.get(i).get(j));
+				ArrayList<String> lines = tweets.get(indexes.get(i).get(j));
+				try {
+					FileUtils.writeLines(new File(sFileName), lines, true);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				tweetCount++;
+			}
+		}
+
+	}
+
+	public static void split(String fileName, int numSplits) {
+
+		LineNumberReader lnr;
+		int numOfLines = 0;
+		try {
+			lnr = new LineNumberReader(new FileReader(new File(fileName)));
+
+			lnr.skip(Long.MAX_VALUE);
+			System.out.println(lnr.getLineNumber());
+			numOfLines = lnr.getLineNumber();
+			// Finally, the LineNumberReader object should be closed to prevent
+			// resource leak
+			lnr.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assert numOfLines > 0; // TODO make sure there are no empty files being
+								// split.
+
+		int minLinesforSplit = numOfLines / numSplits;
+
+		FileReader fr = null;
+		try {
+			fr = new FileReader(fileName);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		int lineCounter = 0;
+		try (BufferedReader br = new BufferedReader(fr)) {
+
+			String sCurrentLine;
+			int fileNum = 1;
+			while ((sCurrentLine = br.readLine()) != null) {
+				//
+				lineCounter++;
+				String sFileName = "/Volumes/LocalDataHD/ps324/Documents/workspace/TwitterNLP/bin/data/daily547_"
+						+ fileNum + ".conll";
+
+				FileUtils.write(new File(sFileName), sCurrentLine + "\n",
+						Charset.defaultCharset(), true);
+
+				System.out.println(sCurrentLine);
+
+				if (lineCounter > minLinesforSplit && sCurrentLine.length() < 1) {
+					lineCounter = 0;
+					fileNum++;
+					System.out.println("split!");
+				}
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	public void byteSplit(String filename) throws Exception {
 		RandomAccessFile raf = new RandomAccessFile(filename, "r");
 		long numSplits = 10; // from user input, extract it from args
